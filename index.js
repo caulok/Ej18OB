454bf7739c917843ee56d78384835b8a36bac388
caulok <ramicaulo@gmail.com>
2022-11-22 21:50:53 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// - En el fichero index.js:
+// - Crea una variable con tu nombre
+// - Crea una variable con tu apellido
+// - Crea un objeto con tu nombre y tu apellido
+// - Almacena el objeto anterior en la SessionStorage
+// - Almacena el objeto anterior en la LocalStorage
+// - Crea una cookie que caduque en 2 minutos con los datos del objeto anterior
+// - Observa en Google Chrome cómo se almacenan los datos en la SessionStorage, LocalStorage y las cookies
+// - Cierra el navegador, comenta las líneas que almacenan SessionStorage, LocalStorage y CookieStorage y vuelve a abrirlo
+// - Observa cómo la SessionStorage está vacía
+// - Observa cómo la LocalStorage sigue manteniendo el objeto que has almacenado antes de cerrar el navegador
+// - Observa cómo la cookie sigue manteniendo el objeto que has almacenado antes, aunque ya está caducado
+
+const nombre = 'Ramiro';
+const apellido = 'Caulo';
+
+const nombreCompleto = {
+    nombre,
+    apellido
+}
+
+const nombreCompletoSession = sessionStorage.setItem('persona', JSON.stringify(nombreCompleto));
+const nombreCompletoLocal = localStorage.setItem('persona', JSON.stringify(nombreCompleto));
+document.cookie = 'nombreCookie=RamiroCookie';
+document.cookie = 'nombreCaducidad=Caducidad;expires=' + new Date(2022,10,23).getTimezoneOffset;
